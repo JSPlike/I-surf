3494a5b6f0687caa91d2291bf7ca192b658cbf02
niley7464 <jyl7464@gmail.com>
2019-07-08 21:46:31 -0700

    pymongo

--- yelin/test.py
+++ yelin/test.py
@@ -2,9 +2,15 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 # client = MongoClient("mongodb://localhost:27017/")
 
+# database name
 db = client["i-surf"]
+
+# collection(table) name
 collection = db.user
+
+# find all
 doc = collection.find()
 
+# print all
 for x in doc:
     print(x)
