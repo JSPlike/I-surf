c2c8f310bab33de0378039ee759a28bbdb3f2649
niley7464 <jyl7464@gmail.com>
2019-07-16 00:09:05 -0700

    0716_modified by yelin

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,43 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import AppStoreScreen from './AppStore';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Root from './client/Root'
+import './index.css'
+import * as serviceWorker from './serviceWorker'
 
-import AppStore from './resources/App Store.png'
-import Calendar from './resources/Calendar Official.png'
-import Clock from './resources/Clock.png'
-import Settings from './resources/Settings.png'
-
-const IconButton = ({img, name}) => {
-    // consolenpm s.log(children);
-  return (
-    <article className="icon">
-      <img src={img} alt="icon" onClick={goAppStoreScreen}></img>
-      <h4>{name}</h4>
-    </article>
-  );
-};
-
-const goAppStoreScreen = () => {
-    console.log("onClick");
-}
-
-const AppList = () => {
-
-  return (
-    <section className="background_ios">
-      <IconButton img={AppStore} name="App Store" canClick="Y"></IconButton>
-      <IconButton img={Calendar} name="Calendar"></IconButton>
-      <IconButton img={Clock} name="Clock"></IconButton>
-      <IconButton img={Settings} name="Settings"></IconButton>
-    </section>
-  );
-}
-
-
-// ReactDOM.render(<AppList />, document.getElementById('root'));
-ReactDOM.render(<AppStoreScreen />, document.getElementById('root'));
+ReactDOM.render(<Root/>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
